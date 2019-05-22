c256de5564f0dc3ea5ae5e26dd2ad89eb8a50860
steelbridge-io <chris@steelbridge.io>
2019-05-21 18:52:07 -0700

    First commit

---
+++ wordpress/pagination.php
@@ -0,0 +1,39 @@
+<?php
+function get_the_post_navigation( $args = array() ) {
+	$args = wp_parse_args(
+		$args,
+		array(
+			'prev_text'          => '%title',
+			'next_text'          => '%title',
+			'in_same_term'       => false,
+			'excluded_terms'     => '',
+			'taxonomy'           => 'category',
+			'screen_reader_text' => __( 'Post navigation' ),
+		)
+	);
+
+	$navigation = '';
+
+	$previous = get_previous_post_link(
+		'<div class="nav-previous">%link</div>',
+		$args['prev_text'],
+		$args['in_same_term'],
+		$args['excluded_terms'],
+		$args['taxonomy']
+	);
+
+	$next = get_next_post_link(
+		'<div class="nav-next">%link</div>',
+		$args['next_text'],
+		$args['in_same_term'],
+		$args['excluded_terms'],
+		$args['taxonomy']
+	);
+
+	// Only add markup if there's somewhere to navigate to.
+	if ( $previous || $next ) {
+		$navigation = _navigation_markup( $previous . $next, 'post-navigation', $args['screen_reader_text'] );
+	}
+
+	return $navigation;
+}
