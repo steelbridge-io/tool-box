755fd92cd6c112e5d22238b3ee9314258abde237
steelbridge-io <chris@steelbridge.io>
2019-07-07 19:30:09 -0700

    First commit

--- custom-meta-color-picker.php
+++ custom-meta-color-picker.php
@@ -40,45 +40,45 @@ $holiday_stored_meta = get_post_meta( $post->ID );
 ob_start();
 ?>
 
-	<!-- === Meta Field Color Picker example as a row of three for different === -->
-<div id="holiday-template" class="holiday-meta-select text-center">
-  <h2 class="admin-font">Holiday Template Custom Colors</h2>
-  <div class="row mt-2618">
-    <div class="col-lg-2">
-      <div class="panel panel-default">
-        <div class="panel-body text-center colorselector">
+  <!-- === Meta Field Color Picker example as a row of three for different === -->
+  <div id="holiday-template" class="holiday-meta-select text-center">
+    <h2 class="admin-font">Holiday Template Custom Colors</h2>
+    <div class="row mt-2618">
+      <div class="col-lg-2">
+        <div class="panel panel-default">
+          <div class="panel-body text-center colorselector">
 
-          <!-- Color Picker Example -->
-          <label for="meta-carousel-bg-color" class="prfx-row-title"><?php _e( 'Carousel BG Color', 'holiday-textdomain' )
-						?></label>
-          <input name="meta-carousel-bg-color" type="text" value="<?php if ( isset ( $holiday_stored_meta['meta-carousel-bg-color'] ) ) echo
-					$holiday_stored_meta['meta-carousel-bg-color'][0]; ?>" class="meta-color" />
+            <!-- Color Picker Example -->
+            <label for="meta-carousel-bg-color" class="prfx-row-title"><?php _e( 'Carousel BG Color', 'holiday-textdomain' )
+              ?></label>
+            <input name="meta-carousel-bg-color" type="text" value="<?php if ( isset ( $holiday_stored_meta['meta-carousel-bg-color'] ) ) echo
+            $holiday_stored_meta['meta-carousel-bg-color'][0]; ?>" class="meta-color" />
+          </div>
         </div>
       </div>
-    </div>
 
-    <div class="col-lg-2">
-      <div class="panel panel-default">
-        <div class="panel-body text-center colorselector">
-          <label for="meta-grid-bg-color" class="prfx-row-title"><?php _e( 'Grid BG Color', 'holiday-textdomain' )
-						?></label>
-          <input name="meta-grid-bg-color" type="text" value="<?php if ( isset ( $holiday_stored_meta['meta-grid-bg-color'])) echo $holiday_stored_meta['meta-grid-bg-color'][0]; ?>" class="meta-grid-bg-color">
+      <div class="col-lg-2">
+        <div class="panel panel-default">
+          <div class="panel-body text-center colorselector">
+            <label for="meta-grid-bg-color" class="prfx-row-title"><?php _e( 'Grid BG Color', 'holiday-textdomain' )
+              ?></label>
+            <input name="meta-grid-bg-color" type="text" value="<?php if ( isset ( $holiday_stored_meta['meta-grid-bg-color'])) echo $holiday_stored_meta['meta-grid-bg-color'][0]; ?>" class="meta-grid-bg-color">
+          </div>
         </div>
       </div>
-    </div>
 
-    <div class="col-lg-2">
-      <div class="panel panel-default">
-        <div class="panel-body text-center colorselector">
-          <label for="meta-color-text" class="prfx-row-title"><?php _e( 'Text Color', 'holiday-textdomain' )?></label>
-          <input name="meta-color-text" type="text" value="<?php if ( isset ( $holiday_stored_meta['meta-color-text'])) echo $holiday_stored_meta['meta-color-text'][0]; ?>" class="meta-color-text">
+      <div class="col-lg-2">
+        <div class="panel panel-default">
+          <div class="panel-body text-center colorselector">
+            <label for="meta-color-text" class="prfx-row-title"><?php _e( 'Text Color', 'holiday-textdomain' )?></label>
+            <input name="meta-color-text" type="text" value="<?php if ( isset ( $holiday_stored_meta['meta-color-text'])) echo $holiday_stored_meta['meta-color-text'][0]; ?>" class="meta-color-text">
+          </div>
         </div>
       </div>
+
     </div>
 
-  </div>
-
-<?php } ?>
+  <?php } ?>
 
 
 
